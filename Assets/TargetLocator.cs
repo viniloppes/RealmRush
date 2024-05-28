e32c9278cfcfc3ad496c65ed053383cc47e3a432
Vinicius Lopes <viniloppes2000@gmail.com>
2024-05-28 00:01:40 -0300

    Implementando arma

---
+++ Assets/TargetLocator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] Transform weapon;
+    [SerializeField] Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<EnemyMover>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimWeapon();
+    }
+
+    void AimWeapon()
+    {
+        weapon.LookAt(target);
+
+    }
+}
--- Assets/Tile/Waypoint.cs
+++ Assets/Tile/Waypoint.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] private bool isPlaceable = false;
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable=false;
+        }
     }
 }
